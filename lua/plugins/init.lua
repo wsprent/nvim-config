d47f316fd1e3bf9994ca1a7a99793a90a250e118
William Sprent <williams@unity3d.com>
2024-01-08 16:54:38 +0100

    neogit + leap

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,8 +2,8 @@ return {
   -- NOTE: First, some plugins that don't require any configuration
 
   -- Git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
+  -- 'tpope/vim-fugitive',
+  -- 'tpope/vim-rhubarb',
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
@@ -151,6 +151,23 @@ return {
       require("nvim-surround").setup()
     end
   },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    config = true
+  },
+  {
+    "ggandor/leap.nvim",
+    config = function ()
+      require('leap').create_default_mappings(true)
+    end
+  }
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
